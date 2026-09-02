e123413bb6bcdace568daea8c48ebce4d256a6de
Minchen Lee <minnndy0301@gmail.com>
2026-09-02 18:33:00 +0800

    deploy: 任選組合改為一般內頁中間挑選（web build）

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "399213003" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3068732791" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

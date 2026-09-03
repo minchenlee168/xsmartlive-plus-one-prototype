5cebf8af78e14433a7d6638fefc16c38dfd03b6d
Minchen Lee <minnndy0301@gmail.com>
2026-09-03 09:47:20 +0800

    deploy: coupon_white.svg 顏色修正（web build）

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3924791378" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "547049953" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

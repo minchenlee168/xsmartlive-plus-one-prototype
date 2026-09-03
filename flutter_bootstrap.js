cb7fb493f7bc7bf70a5f363850e31725c9cf1126
Minchen Lee <minnndy0301@gmail.com>
2026-09-03 13:58:58 +0800

    deploy: 訂單金額明細 + 已出貨字色（web build）

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2755258758" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3893121639" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
